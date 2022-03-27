0585c62b78d8486ba2a8aa9fbde524ebf6678fc5
Oyaronet <oyarojared006@gmail.com>
2022-03-27 13:28:50 +0300

    Database bug fix

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, redirect, flash
 from forms import RegistrationForm, UserLoginForm, AdminLoginForm
-from database import users, engine, meta
+# from database import users, engine, meta
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import insert, select, update, delete, and_, or_
 
 app = Flask(__name__)
@@ -8,6 +9,20 @@ app.config["SECRET_KEY"] = "718318cf14f5c2c3568b7bb620e6cc32"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://epmswlywsgfgec:a471edc1aa9d0338b9cb4de98802af9a10189f6a\
 576b1311fa17300dbc26bf7a@ec2-44-194-92-192.compute-1.\
 amazonaws.com:5432/deramp098sjfju"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        """ string representation of the user table """
+        return f'User({self.username}, {self.email})'
+
  
 @app.route("/", methods=["GET", "POST"])
 def register():
@@ -15,12 +30,11 @@ def register():
     if form.validate_on_submit():
         try:
             """ send user data to the database """
-            users.create(engine, checkfirst=True)
-            engine.connect().execute(users.insert(), {
-                "username" : form.username.data,
-                "email": form.email.data,
-                "password": form.password.data
-            })
+            db.create_all()
+            user = User(username=form.username.data, \
+                  email=form.email.data, password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
             flash(f'Account created successfully for {form.username.data}!\
                     Please log in to continue!', 'success')
             return redirect(url_for('user_login'))
@@ -35,11 +49,10 @@ def user_login():
     form = UserLoginForm()
     if form.validate_on_submit():
         """ Log in User with Correct Credentials """ 
-        user = select([users]).where(and_(
-            users.c.email == form.email.data,
-            users.c.password == form.password.data
-        ))
-        user = engine.connect().execute(user).first()
+        user = User.query.filter(and_(
+            User.email == form.email.data,
+            User.password == form.password.data
+        )).first()
         if user:
             return render_template('userdashboard.html', user=user)
         else:
