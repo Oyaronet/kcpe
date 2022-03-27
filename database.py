ffbb98a0a5e5c9bd426e1f062dbc2183cecab5c1
Oyaronet <oyarojared006@gmail.com>
2022-03-27 10:36:45 +0300

    db bug fix

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy import MetaData, create_engine
 from sqlalchemy import Table, Column, String, Integer, ForeignKey
  
 meta = MetaData()
-engine = create_engine('sqlite:///site.db', echo=False)
+engine = create_engine('postgres://epmswlywsgfgec:a471edc1aa9d0338b9cb4de98802af9a10189f6a\
+576b1311fa17300dbc26bf7a@ec2-44-194-92-192.compute-1.\
+amazonaws.com:5432/deramp098sjfju')
 
 users = Table('users', meta,
     Column('id', Integer, primary_key=True),
